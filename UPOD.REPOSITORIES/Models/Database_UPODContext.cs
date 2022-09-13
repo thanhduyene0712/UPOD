48ac75ed1a8e141c9315540b29dd946338162966
Thanh Duyên Trần Nguyễn <tranduyen07121999@gmail.com>
2022-09-13 13:00:02 +0700

    chua fix dc appsettings

--- UPOD.REPOSITORIES/Models/Database_UPODContext.cs
+++ UPOD.REPOSITORIES/Models/Database_UPODContext.cs
@@ -37,14 +37,15 @@ namespace UPOD.REPOSITORIES.Models
         public virtual DbSet<Technican> Technicans { get; set; } = null!;
         public virtual DbSet<Ticket> Tickets { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=Database_UPOD;User ID=sa;Password=THANHDUYEN07121999;Trusted_Connection=True;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=Database_UPOD;User ID=sa;Password=THANHDUYEN07121999;Trusted_Connection=True;");
+//                //optionsBuilder.UseSqlServer("Server=13.232.213.53,1433;Initial Catalog=Database_UPOD;User ID=sa;Password=Loyalty@Program;Trusted_Connection=True;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
