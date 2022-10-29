b4f85097105119cfadc16e657cf0ec92869e9140
Thanh Duyên Trần Nguyễn <tranduyen07121999@gmail.com>
2022-10-30 03:30:49 +0700

    getall roles

--- UPOD.REPOSITORIES/ResponseModels/RoleResponse.cs
+++ UPOD.REPOSITORIES/ResponseModels/RoleResponse.cs
@@ -11,8 +11,5 @@ namespace UPOD.REPOSITORIES.ResponseModels
         public Guid id { get; set; }
         public string? code { get; set; }
         public string? role_name { get; set; }
-        //public bool? is_delete { get; set; }
-        //public DateTime? create_date { get; set; }
-        //public DateTime? update_date { get; set; }
     }
 }
